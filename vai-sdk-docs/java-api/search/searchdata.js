122445bd05233de7530a4731d5f90481f48ba8d2
lemon <leoeco2000@qq.com>
2021-08-04 03:11:26 +0800

    添加api页面

---
+++ vai-sdk-docs/java-api/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "abdegimnopstvw",
+  1: "abstv",
+  2: "degimnopstw",
+  3: "adpt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "全部",
+  1: "类",
+  2: "函数",
+  3: "变量"
+};
+
